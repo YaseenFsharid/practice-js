d9d4c5040cf1f2d312ba3b32da26640dde550bc7
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-05 09:13:51 +0200

    javascript dynamic web page

---
+++ addcontent.js
@@ -0,0 +1,14 @@
+var today =new Date();
+var hourNow =today.getHours();
+var greeting ;
+if (hourNow >18){
+    greeting ='good eveninig';
+} else if(hourNow > 12){
+    greeting ='good afternoon';
+}else if(hourNow > 0){
+    greeting = 'good morning';
+
+}else{
+    greeting = 'welcome';
+}
+document.write('<h3>'+ greeting + '</h3>')
