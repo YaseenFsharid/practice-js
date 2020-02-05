dedf4ae9ad083918338f26fdc5ae11471712ff16
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-05 13:22:24 +0200

    adding function

--- addcontent.js
+++ addcontent.js
@@ -1,14 +1,22 @@
-var today =new Date();
-var hourNow =today.getHours();
-var greeting ;
-if (hourNow >18){
-    greeting ='good eveninig';
-} else if(hourNow > 12){
-    greeting ='good afternoon';
-}else if(hourNow > 0){
-    greeting = 'good morning';
-
-}else{
-    greeting = 'welcome';
+function greeting() {
+    //input
+    var today =new Date();
+    var hourNow =prompt("enter your greeting");
+    var greeting;
+    // processing 
+    if (hourNow > 0 && hourNow < 12 ){
+        greeting ='good morning';
+    } else if(hourNow > 12 && hourNow < 18){
+        greeting ='good afternoon';
+    }else if(hourNow > 18 && hourNow <=24){
+        greeting = 'good evening ';
+    
+    }else{
+        greeting = 'welcome';
+    }        
+    // output
+    return greeting;
 }
+
+var greeting = greeting();
 document.write('<h3>'+ greeting + '</h3>')
